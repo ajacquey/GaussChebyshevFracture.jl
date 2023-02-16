176d85fe8abc3a68a06546bc3e379cac961d8470
Documenter.jl <documenter@juliadocs.github.io>
2023-02-16 19:33:47 +0000

    build based on 6ef9ff5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GaussChebyshevFracture","category":"page"},{"location":"#GaussChebyshevFracture","page":"Home","title":"GaussChebyshevFracture","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GaussChebyshevFracture.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GaussChebyshevFracture]","category":"page"}]
+}
